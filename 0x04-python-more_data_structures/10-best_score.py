1faa0fbe16fa7f1d2b1db19f40917b4ff2d6df45
henokgetachewnibret <henokgetachew703@gmail.com>
2024-03-22 02:16:02 -0700

    Done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary:
-        my_list = list(a_dictionary.keys())
-        score = 0
-        leader = ""
-        for i in my_list:
-            if a_dictionary[i] > score:
-                socre = a_dictionary[i]
-                leader = i
-        return leader
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key = a_dictionary.get))
