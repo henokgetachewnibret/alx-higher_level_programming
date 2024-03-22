11e5b14cf680eb4f7b7c8dbdc8008f0730fab153
henokgetachewnibret <henokgetachew703@gmail.com>
2024-03-22 01:47:12 -0700

    Done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 def multiply_by_2(a_dictionary):
     new_dico = {}
     if a_dictionary:
-        for key, value in a_dictionary.item():
+        for key, value in a_dictionary.items():
             new_dico.update({key: value * 2})
         return new_dico
