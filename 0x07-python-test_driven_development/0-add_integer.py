0cb29204cae0701cc52e374726ee560d4f3310fc
henokgetachewnibret <henokgetachew703@gmail.com>
2024-04-25 08:36:48 -0700

    Done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Module 0-add_integer
-contain one methon and accept two values of int or float type
-and cast them to int before adding
-Return an int sum
-"""
 def add_integer(a, b=98):
+    """
+    Return the summation of two argument
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
